57452847d2354bb0b14363c0eab0072a47c60c53
krumaska <51515055+krumaska@users.noreply.github.com>
2019-11-10 20:07:56 +0900

    stack.c add func

--- stack/stack.c
+++ stack/stack.c
@@ -1,9 +1,36 @@
 #include "stack.h"
 
 // implement functions behind
-void    initialize(stack *stk){}
-void    push(data d, stack *stk){}
-data    pop(stack *stk){}
-data    top(stack *stk){}
-boolean empty(const stack *stk){}
-boolean full(const stack *stk){}
+elem *head;
+void initialize(stack *stk){
+    head = malloc(sizeof(struct elem));
+    head -> next = EMPTY;
+}
+void push(data d, stack *stk){
+    elem *tmp = (elem *)malloc(sizeof(elem));
+    tmp -> d = d;
+    tmp -> next = stk -> top;
+    stk -> top = tmp;
+    stk -> cnt += 1;
+}
+data pop(stack *stk){
+    char rrr;
+    elem *tmp;
+    tmp = stk -> top;
+    stk -> top = tmp -> next;
+    stk -> cnt -= 1;
+    rrr = tmp -> d;
+    free(tmp);
+    return rrr;
+}
+data top(stack *stk){
+    return stk -> top;
+}
+boolean empty(const stack *stk){
+    if (stk -> top == EMPTY) return false;
+    else return true;
+}
+boolean full(const stack *stk){
+    if (stk -> cnt == FULL) return true;
+    else return false;
+}
