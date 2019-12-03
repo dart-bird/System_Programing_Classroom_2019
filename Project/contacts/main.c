eaa7295842f7ead2acdafef2212191cea474ebd5
krumaska <51515055+krumaska@users.noreply.github.com>
2019-12-03 14:23:47 +0900

    Update main.c

--- Project/contacts/main.c
+++ Project/contacts/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "func.c"
+#include "data.h"
+
 int menu();
-void Registration();
-void ShowAll();
-void Delete();
-void FindByBirth();
-char *str;
+
+contactData cad; //init C ont A ct D ata
+
 int main(){
-    str = (char*)malloc(sizeof(char) * 8);
     while (menu()){ //0을 받으면 종료되는 형태 exit_code 값
         menu();
-
     }
     
     return 0;
@@ -23,57 +22,28 @@ int menu() {
     puts("*****Menu*****");
         puts("1. Registration, 2. ShowAll, 3. Delete, 4. FindByBirth, 5. Exit");
         printf("Enter_the_menu_number: ");
-        //fflush(stdin); //remove enter buffer
         scanf("%d",&menu_num);
-        //fflush(stdin); //remove enter buffer
         switch (menu_num){
             case 1: //1. Registration
-                printf("this menu %d\n",menu_num);
-                Registration();
+                //printf("this menu %d\n",menu_num);
+                Registration(&cad);
                 break;
             case 2: //2. ShowAll
-                printf("this menu %d\n",menu_num);
+                //printf("this menu %d\n",menu_num);
+                ShowAll();
                 break;
             case 3: //3. Delete
-                printf("this menu %d\n",menu_num);
+                //printf("this menu %d\n",menu_num);
+                Delete();
                 break;
             case 4: //4. FindByBirth
-                printf("this menu %d\n",menu_num);
+                //printf("this menu %d\n",menu_num);
+                FindByBirth();
                 break;
             case 5: //5. Exit
                 exit_code = 0; // 종료 조건 변수에 1
-                printf("this menu %d\n",menu_num);
-                puts("Bye");
+                //printf("this menu %d\n",menu_num);
                 break;
     }
     return exit_code;
-}
-
-void Registration() {
-    char *nameStr = (char*)malloc(sizeof(char) * 20);
-    char *phoneStr = (char*)malloc(sizeof(char) * 15);
-    char *birthStr = (char*)malloc(sizeof(char) * 8); 
-    
-    printf("Name:");
-    scanf("%s",nameStr);
-    printf("Phone_number:");
-    scanf("%s",phoneStr);
-    printf("Birth:");
-    scanf("%s",birthStr);
-
-    printf("Name: %s", nameStr);
-    printf("Phone_number: %s", phoneStr);
-    printf("Birth: %s\n", birthStr);
-}
-
-void ShowAll() {
-
-}
-
-void Delete() {
-
-}
-
-void FindByBirth() {
-
 }
