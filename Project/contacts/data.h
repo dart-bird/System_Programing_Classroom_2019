630bc32f4e28ed94b57d67f105dbfc26e9aa5903
krumaska <51515055+krumaska@users.noreply.github.com>
2019-12-04 22:22:45 +0900

    Update data.h

--- Project/contacts/data.h
+++ Project/contacts/data.h
@@ -1,23 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #ifndef DATA_H
 #define DATA_H
-
+#include <stdio.h>
+#include <stdlib.h>
 struct contactData {
   char *name;
   char *phoneNumber;
   char *birth;
   struct contactData *next; //next contactData node
 };
-
 struct contactData **data;
-
-void(*func_p(const int opCode))();
 
 void Registration();
 void ShowAll();
 void Delete();
 void FindByBirth();
-
 #endif
